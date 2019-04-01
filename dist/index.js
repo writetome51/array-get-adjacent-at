a4d00e2ae0134f0d76c746b7c7102cead87c9eb1
Steve Thompson <writetome51@gmail.com>
2019-03-31 20:39:51 -0600

    Updated dependencies. Upped version to 1.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var _arraySlice_1 = require("./privy/_arraySlice");
-var errorIfIndex_howMany_andArray_areNotValid_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfIndex_howMany_andArray_areNotValid");
-var errorIfArrayTooShortToMeetAdjacentItemsRequest_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfArrayTooShortToMeetAdjacentItemsRequest");
+var error_if_array_too_short_to_meet_adjacent_items_request_1 = require("error-if-array-too-short-to-meet-adjacent-items-request");
+var error_if_index_how_many_or_array_are_not_valid_1 = require("error-if-index-how-many-or-array-are-not-valid");
 // This function is a strict type-checking version of _arraySlice().
-// If either startingIndex or howMany are not integers, or array is not an array,
-// an error is triggered.
 // startingIndex can be negative or positive.
 function getAdjacentAt(startingIndex, howMany, array) {
-    errorIfIndex_howMany_andArray_areNotValid_1.errorIfIndex_howMany_andArray_areNotValid(startingIndex, howMany, array);
-    errorIfArrayTooShortToMeetAdjacentItemsRequest_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howMany, array);
+    error_if_index_how_many_or_array_are_not_valid_1.errorIfIndex_howMany_orArray_areNotValid(startingIndex, howMany, array);
+    error_if_array_too_short_to_meet_adjacent_items_request_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howMany, array.length);
     if (howMany === 0)
         return [];
     if (startingIndex + howMany === 0)
--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,18 @@
 import { _arraySlice } from './privy/_arraySlice';
-import {errorIfIndex_howMany_andArray_areNotValid} 
-	from '@writetome51/array-and-index-validation/errorIf/errorIfIndex_howMany_andArray_areNotValid';
 import { errorIfArrayTooShortToMeetAdjacentItemsRequest }
-	from '@writetome51/array-and-index-validation/errorIf/errorIfArrayTooShortToMeetAdjacentItemsRequest';
+	from 'error-if-array-too-short-to-meet-adjacent-items-request';
+import { errorIfIndex_howMany_orArray_areNotValid }
+	from 'error-if-index-how-many-or-array-are-not-valid';
 
 
 // This function is a strict type-checking version of _arraySlice().
-// If either startingIndex or howMany are not integers, or array is not an array,
-// an error is triggered.
 // startingIndex can be negative or positive.
 
-export function getAdjacentAt(startingIndex: number, howMany: number, array): any[] {
-	errorIfIndex_howMany_andArray_areNotValid(startingIndex, howMany, array);
-	errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howMany, array);
+export function getAdjacentAt(startingIndex, howMany, array): any[] {
+
+	errorIfIndex_howMany_orArray_areNotValid(startingIndex, howMany, array);
+	errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, howMany, array.length);
+
 	if (howMany === 0) return [];
 	if (startingIndex + howMany === 0) return _arraySlice(startingIndex, array.length, array);
 	else return _arraySlice(startingIndex, startingIndex + howMany, array);
